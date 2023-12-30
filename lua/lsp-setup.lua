e6f85b81c60284e190379eda43f038b978344cce
nicolasnk <nkierenicolas@gmail.com>
2023-12-30 11:24:06 -0800

    Adding new configuration for dart lsp

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -66,6 +66,8 @@ require('which-key').register({
 require('mason').setup()
 require('mason-lspconfig').setup()
 
+
+
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
@@ -77,10 +79,10 @@ require('mason-lspconfig').setup()
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+
 
   lua_ls = {
     Lua = {
@@ -98,6 +100,41 @@ require('neodev').setup()
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+-- Dart bundles their LSP server with the runtime executable itself. Thus dart lsp is not included within Mason
+-- No need for mason to install the lsp as dart lsp is already installed if you're developping using dart
+local lspconfig = require('lspconfig')
+local util = lspconfig.util
+require("lspconfig").dartls.setup({
+            cmd = { "dart", "language-server", "--protocol=lsp" },
+            filetypes = { "dart" },
+            init_options = {
+                closingLabels = true,
+                flutterOutline = true,
+                onlyAnalyzeProjectsWithOpenFiles = false,
+                outline = true,
+                suggestFromUnimportedLibraries = true,
+            },
+            root_dir = function(fname)
+              -- Check if the file is inside the .pub-cache
+              if string.find(fname, "/.pub-cache/") then
+                  -- Find the .pub-cache root 
+                  return os.getenv("HOME") .. "/.pub-cache"
+                else
+                  -- Default project root finding logic
+                  return util.root_pattern("pubspec.yaml")(fname) or
+                          util.path.dirname(fname)
+                end
+              end,
+            settings = {
+                dart = {
+                    completeFunctionCalls = true,
+                    showTodos = true,
+                },
+            },
+            on_attach = on_attach,
+            capabilities = capabilities,
+        })
 
 -- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
