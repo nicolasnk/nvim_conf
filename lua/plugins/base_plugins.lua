888b9dd0d4839fd16f2a038a7eb24915d17b0c87
nicolasnk <nkierenicolas@gmail.com>
2024-01-06 02:47:00 -0800

    Revamping neovim config, making more modularizable for me

---
+++ lua/plugins/base_plugins.lua
@@ -0,0 +1,33 @@
+return {
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+  },
+
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim', opts = {} },
+
+  -- Help with comemnting visual regiones/lines
+  -- using `gc`
+  { 'numToStr/Comment.nvim', opts = {} },
+
+}
---
+++ lua/plugins/indent_blankline.lua
@@ -0,0 +1,10 @@
+-- Adding indentation guidelines to neovim
+
+return {
+  -- Add indentation guides even on blank lines
+  'lukas-reineke/indent-blankline.nvim',
+  -- Enable `lukas-reineke/indent-blankline.nvim`
+  -- See `:help ibl`
+  main = 'ibl',
+  opt = {},
+}
